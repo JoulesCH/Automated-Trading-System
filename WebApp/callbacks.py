eb8e93a5b79468e060a03a2250adc3620f7b05e4
Joules <joules.hdz@gmail.com>
2021-09-30 00:21:59 -0500

    heroku added

--- WebApp/callbacks.py
+++ WebApp/callbacks.py
@@ -14,6 +14,7 @@ import plotly.express as px
 import pandas as pd
 from dash import dcc
 import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
 
 @appD.callback(
     Output("navbar-collapse", "is_open"),
@@ -26,31 +27,57 @@ def toggle_navbar_collapse(n, is_open):
     return is_open
 
 @appD.callback(Output('output-data-upload', 'children'),
+              Output('uploadFile', 'children'),  
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
 def update_output(content, filename, last_modified):
     if not filename:
-        return None
+        raise PreventUpdate
     elif filename[-4:] not in ['.txt', '.csv']:
-        return html.P('Tipo de archivo no permitido', style = {'color':'red'})
+        return dbc.Alert("Tipo de archivo no permitido", color="danger"),
     else:
         content_type, content_string = content.split(',')
         decoded = base64.b64decode(content_string).decode('utf-8').replace('\r','')
         try:
             listDecoded = [  float(value) for value in decoded.split('\n')]
         except:
-            return html.P('Hay un dato no válido', style = {'color':'red'})
+            return dbc.Alert("Hay un dato no válido", color="danger"),
 
-        df = pd.Series(listDecoded)
         fig = go.Figure(data = [go.Scatter(x=list(range(len(listDecoded))), y=listDecoded)])
         fig.add_trace(go.Scatter(x=list(range(len(listDecoded))), y=listDecoded, mode="markers") )
         fig.update_xaxes(rangeslider_visible=True)
+        fig.layout.height = 700
         return html.Div([
-            dcc.Graph(figure = fig, animate  =  True),
-            html.Div(
-                dbc.Button('¡Empezar a tradear!', color="primary", className="mr-1"),
+            dcc.Graph(figure = fig, animate  =  True, id = 'tradeGraph'),
+            html.Div([
+                dbc.FormGroup(
+                    [
+                        dbc.Label("Capital inicial:"),
+                        dbc.Input(placeholder="Valor entero mayor a 0", type="number", min = 0, value = 2000.00),
+                    ], style = {'width':'40%','margin-left':'auto', 'margin-right':'auto'}, id = 'formCapital'
+                ),
+                dbc.Button('¡Empezar a tradear!', color="primary", className="mr-1", id = 'beginButton'),
+                ],
                 style = {'text-align':'center'}
             )
-        ])
-    
+        ]), html.Div([
+                dbc.Alert([
+                        html.H4("Datos cargados con éxito"),
+                        html.Hr(),
+                        "Revisa la gráfica y desliza hasta abajo para ingresar los datos faltantes", 
+                    ], color="primary",duration=5000,
+                )
+        ])   
+
+@appD.callback(Output('tradeGraph', 'figure'), 
+                Output('beginButton', 'children'),
+                Output('formCapital', 'children'),
+                Input("beginButton","n_clicks"))
+def beginTrading(n):
+    if not n:
+        raise PreventUpdate
+    else:
+        return [go.Figure(data = [go.Scatter(x=list(range(len([1,2,3,4]))), y=[2,4,6,8])]), 
+                html.A('Ingresar otro archivo', href = '/dash/',style = {'color':'white', 'text-decoration':'None'}), 
+                None]
--- WebApp/resources/dashTemplates/loadFile.py
+++ WebApp/resources/dashTemplates/loadFile.py
@@ -3,32 +3,34 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 load = html.Div([
-    html.H2('Para empezar ingresa un archivo con los datos:'),
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Agarra y arrastra o  ',
-            html.A('selecciona un archivo ')
-        ]),
-        style={
-            'width': '70%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px',
-            'margin-left':'auto',
-            'margin-right':'auto',
-            'margin-top':'30px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=False,
-    ),
+    html.Div([
+        html.H2('Para empezar ingresa un archivo con los datos:'),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Agarra y arrastra o  ',
+                html.A('selecciona un archivo ')
+            ]),
+            style={
+                'width': '70%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px',
+                'margin-left':'auto',
+                'margin-right':'auto',
+                'margin-top':'30px'
+            },
+            # Allow multiple files to be uploaded
+            multiple=False,
+        ),
+    ], id = 'uploadFile'),
     html.Div(
         dbc.Spinner(
             html.Div(id='output-data-upload'),
         color="primary"),
-    style = {'margin-top':50})
+    style = {'margin-top':5})
 ])
