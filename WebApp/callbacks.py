bf61792dcf00149de05927e82f409378cb4edc3b
Joules <joules.hdz@gmail.com>
2021-10-01 10:26:29 -0500

    size of resume

--- WebApp/callbacks.py
+++ WebApp/callbacks.py
@@ -92,6 +92,7 @@ def beginTrading(n, data):
         fig.layout.height = 700
         fig.update_xaxes(rangeslider_visible=True)
         capital_ini = 2000
+        capital_final = 4500
         data = [
             {'Movimiento': '123a','Volumen':3, 'OpenValue':1183, 'OpenIdx':0, 'CloseValue':1159.45,'CloseIdx':1, 'Balance':500},
             {'Movimiento': '123b','Volumen':3, 'OpenValue':1193.8,'OpenIdx':3, 'CloseValue':1195.82, 'CloseIdx':4, 'Balance':500},
@@ -105,7 +106,7 @@ def beginTrading(n, data):
             {'Movimiento': '123j','Volumen':3, 'OpenValue':1183, 'OpenIdx':0, 'CloseValue':1159.45,'CloseIdx':1, 'Balance':-500},
             {'Movimiento': '123k','Volumen':3, 'OpenValue':1193.8,'OpenIdx':3, 'CloseValue':1195.82, 'CloseIdx':4, 'Balance':500},
         ]
-        measures = ["absolute"]; x = ["Capital"]; y=[capital_ini]; text = ["Capital"]
+        measures = ["absolute"]; x = ["Capital Inicial"]; y=[capital_ini]; text = ["Capital Inicial"]
         for move in data:
             fig.add_trace(go.Scatter(x = [move['OpenIdx'], move['CloseIdx']], y= [move['OpenValue'], move['CloseValue']],
                 name = move['Movimiento'],
@@ -115,6 +116,10 @@ def beginTrading(n, data):
             x.append(move['Movimiento'])
             y.append(move['Balance'])
             text.append(f'{move["Balance"]}')
+        measures.append('total')
+        x.append('Capital Final')
+        y.append(capital_final)
+        text.append('Capital Final')
         df = pd.DataFrame(data)
         balances = []
         for value in df['Balance']:
@@ -123,11 +128,11 @@ def beginTrading(n, data):
 
         df['Balance'] = balances
         fig2 = make_subplots(
-            rows=2, cols=1,
-            shared_xaxes=True,
-            vertical_spacing=0.03,
-            specs=[[{"type": "waterfall"}],
-                [{"type": "scatter"}],
+            rows=2, cols=2,
+            #shared_xaxes=True,
+            vertical_spacing=0.1,
+            specs=[[{'type':'domain'}, {'type':'domain'}],
+                [{"type": "waterfall", "colspan": 2}, None],
             ]
         )
         fig2.add_trace(
@@ -139,7 +144,26 @@ def beginTrading(n, data):
                 text = text,
                 y =y,
                 connector = {"line":{"color":"rgb(63, 63, 63)"}},
-            ), row=1, col=1
+            ), row=2, col=1
+        )
+        labels = ["US", "China", "European Union", "Russian Federation", "Brazil", "India",
+          "Rest of World"]
+
+        # Create subplots: use 'domain' type for Pie subplot
+        #fig3 = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+        fig2.add_trace(go.Pie(labels=labels, values=[16, 15, 12, 6, 5, 4, 42], name="GHG Emissions", hole=.4,),
+                    1, 1)
+        fig2.add_trace(go.Pie(labels=labels, values=[27, 11, 25, 8, 1, 3, 25], name="CO2 Emissions", hole=.4,),
+                    1, 2)
+
+        fig2.update_layout(
+            showlegend=False,
+            title_text="Global Emissions 1990-2011",
+            autosize=True,
+            height=500
+            # Add annotations in the center of the donut pies.
+            #annotations=[dict(text='GHG', x=0.18, y=0.5, font_size=20, showarrow=False),
+            #            dict(text='CO2', x=0.82, y=0.5, font_size=20, showarrow=False)]
         )
         return [fig, 
                 html.A('Ingresar otro archivo', href = '/dash/',style = {'color':'white', 'text-decoration':'None'}), 
@@ -154,14 +178,14 @@ def beginTrading(n, data):
                                                             bordered=True, hover=True,
                                                             size = 'sm',
                                 ),
-                            style={'height': '300px', 'overflowY': 'auto'}                            
+                            style={'height': '500px', 'overflowY': 'auto'}                            
                             ),
                         ]),
                         dbc.Col([
                             html.H2('Resumen'),
-                            dcc.Graph(figure = fig2)
+                            dcc.Graph(figure = fig2, config={'autosizable':True})
                         ]),
-                    ]),
-                    html.H2('Gráfica de movimientos'),
+                    ], style={'height':'500px','margin-bottom':'40px'}),
+                    html.H2('Gráfica de movimientos', style={'margin-top':'20px'}),
                 ])
                 ]
